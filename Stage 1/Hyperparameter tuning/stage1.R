b942400b03e8ae57733437ada45bf624ce267529
Dania M. Rodriguez <dmrodz@gmail.com>
2024-08-11 17:57:39 -0400

    Update stage1.R

--- Stage 1/Hyperparameter tuning/stage1.R
+++ Stage 1/Hyperparameter tuning/stage1.R
@@ -6,13 +6,13 @@ path = #your/path/here
 setwd(path)
 # Setting connection
 print("Setting up connection...")
-con <- file(paste0(path, "/logs/stage1_log - ", Sys.Date(), ".log"))
+con <- file(paste0(path, "/logs/stage1_hp_log - ", Sys.Date(), ".log"))
 print("Producing sink...")
 sink(con, append=TRUE)
 sink(con, append=TRUE, type="message")
 # Run code
 print("Running script...")
-source(paste0(path, "/stage1_script.R"), encoding="utf-8", 
+source(paste0(path, "/stage1_hp_script.R"), encoding="utf-8", 
        max.deparse.length = 10000)
 print("Script ran successfully!")
 print("Done.")
